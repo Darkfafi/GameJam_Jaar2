ae6e6aef5cadab8e30a2bb18639b7061d5ba1176
Rick Musters <rickjoshuamusters@hotmail.com>
2014-08-29 09:51:45 +0200

    levelCreator

---
+++ Assets/_Script/_Xml/XmlData.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+[XmlRoot("data")]
+public class XmlData {
+	
+	[XmlArray("levels")]
+	[XmlArrayItem("level")]
+	public List<XmlLevel> levels = new List<XmlLevel>();
+}
