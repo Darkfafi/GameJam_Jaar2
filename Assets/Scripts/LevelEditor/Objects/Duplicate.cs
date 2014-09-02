8481f3b6d68a11a329023be8faecb20ef74bae8a
Rick Musters <rickjoshuamusters@hotmail.com>
2014-09-02 14:02:37 +0200

    levelEditorBegin

---
+++ Assets/Scripts/LevelEditor/Objects/Duplicate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Duplicate : MonoBehaviour {
+
+	void Update () {
+
+		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+		if(hit.collider !=null)
+		{
+			Debug.Log("nice");
+		}
+
+	}
+}
